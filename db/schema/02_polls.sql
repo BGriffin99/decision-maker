ff83399d729e4f9be08d9aeb808c41ab6f820d84
David Luc <luc.ky@me.com>
2023-05-07 15:33:57 -0400

    Change link columns to default to a random string

--- db/schema/02_polls.sql
+++ db/schema/02_polls.sql
@@ -3,6 +3,6 @@ CREATE TABLE polls (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  user_link VARCHAR(255) NOT NULL,
-  submission_link VARCHAR(255) NOT NULL
+  user_link VARCHAR(32) NOT NULL UNIQUE DEFAULT md5(random()::text),
+  submission_link VARCHAR(32) NOT NULL UNIQUE DEFAULT md5(random()::text)
 );
